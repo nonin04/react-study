a709481d2c4241400cd1b91d293f5223ccae4789
nonin04 <nonin.vctr04@gmail.com>
2025-08-12 23:58:26 +0900

    study:変数の宣言

--- src/index.js
+++ src/index.js
@@ -9,3 +9,29 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+
+
+//--udemy-reactの教材より-------------------------------
+var val1 = "var変数";
+
+val1 = "varは上書きできる";
+// var変数は上書きが可能
+console.log(val1);
+
+//--------
+
+let val2 = "var変数2";
+console.log(val2)
+
+val2 = "よろしく";
+console.log(val2);
+
+//--------
+
+const val3 = "普遍な変数";
+console.log(val3)
+
+val3 = "可変かな？";
+console.log(val3);
