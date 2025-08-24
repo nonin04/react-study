6b54c75e0238d5e9f62f5dc825aa16b571887a56
nonin04 <nonin.vctr04@gmail.com>
2025-08-24 10:29:05 +0900

    study:useStateとhookの使い方

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import { ColorfulMessage } from "./ColorfulMessage";
 
 
@@ -5,15 +6,19 @@ import { ColorfulMessage } from "./ColorfulMessage";
 //コンポーネントは一つのタグで囲われている必要がある(以下の場合だとdivで囲われていない状態だとエラーになる)
 //↑react特有の空タグを入れることで囲いが不要になる(フラグメントを利用する)
 export const App = () => {
+  const [num, setNum] = useState(0);
 
-
-  const onClickBtn = () => alert();
+  const onClickBtn = () => {
+    setNum((prev) => prev + 1);
+    setNum((prev) => prev + 1);
+  }
 
   return (
     <>
       <ColorfulMessage color="blue" message="コンチわ" />
       <p>お元気ですか？</p>
-      <button onClick={onClickBtn}>ボタン</button>
+      <button onClick={onClickBtn}>カウントアップ</button>
+      <p>{num}</p>
     </>
   )
 };
