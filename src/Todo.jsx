fc403b17b60a88c869479f251ea5201565b33f8b
nonin04 <nonin.vctr04@gmail.com>
2025-09-03 09:05:02 +0900

    study:react版todoアプリ完成

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -8,11 +8,13 @@ import { ColorfulMessage } from "./ColorfulMessage";
 
 export const Todo = () => {
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState(["ああ", "いい"]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
   const [completeTodos, setCompleteTodos] = useState([]);
+  
   const onChangeTodoText = (e) => {
     setTodoText(e.target.value)
   }
+  
   const onClickAdd = () => {
     if (todoText === "") return; 
     const newTodos = [...incompleteTodos, todoText];
@@ -20,23 +22,49 @@ export const Todo = () => {
     setTodoText(""); 
   };
 
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1)
+    setIncompleteTodos(newTodos);
+  }
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1)
+
+    const newCompletetodos = [...completeTodos, incompleteTodos[index]];
+
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompletetodos);
+  }
+
+  const onClickBack = (index) => {
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    completeTodos.splice(index, 1)
+
+    const newCompletetodos = [...completeTodos];
+
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompletetodos);
+  }
+ 
   return (
     <>
-      <div class="mx-4 my-4 flex flex-col gap-2">
-        <div class="inputarea bg-blue-400 py-4 px-2 rounded-xl">
+      <div className="mx-4 my-4 flex flex-col gap-2">
+        <div className="inputarea bg-blue-400 py-4 px-2 rounded-xl">
           <input placeholder="TODOを入力" value={todoText} onChange={onChangeTodoText} className="px-4 py-1 rounded-xl bg-white"/>
           <button className="px-4 py-1 ml-2 bg-neutral-100 rounded-xl hover:bg-neutral-300" onClick={onClickAdd}>追加</button>
         </div>
-        <div class="uncompletearea px-2 py-2 border-2 border-blue-400 rounded-xl">
-          <p class="px-4 title text-lg font-bold text-neutral-600">未完了のタスク</p>
-          <ul class="my-4">
+        <div className="uncompletearea px-2 py-2 border-2 border-blue-400 rounded-xl">
+          <p className="px-4 title text-lg font-bold text-neutral-600">未完了のタスク</p>
+          <ul className="my-4">
 
-            {incompleteTodos.map((todo) => (
+            {incompleteTodos.map((todo, index) => (
                 <li key={todo}>
-                  <div class="flex">
+                  <div className="flex">
                     <p>{todo}</p>
-                    <button class="px-4 py-1 ml-1 bg-neutral-200 rounded-xl hover:bg-blue-300">完了</button>
-                    <button class="px-4 py-1 ml-1 bg-neutral-200 rounded-xl hover:bg-blue-300">削除</button>
+                    <button onClick={() => onClickComplete(index)} className="px-4 py-1 ml-1 bg-neutral-200 rounded-xl hover:bg-blue-300">完了</button>
+                    <button onClick={() => onClickDelete(index)} className="px-4 py-1 ml-1 bg-neutral-200 rounded-xl hover:bg-blue-300">削除</button>
                   </div>
                 </li>
             ))}
@@ -44,16 +72,15 @@ export const Todo = () => {
           </ul>
         </div>
 
-        <div class="completearea px-2 py-2 border-2 border-blue-400 bg-blue-100 rounded-xl">
-          <p class="px-4 title text-lg font-bold text-neutral-600">完了済みのタスク</p>
+        <div className="completearea px-2 py-2 border-2 border-blue-400 bg-blue-100 rounded-xl">
+          <p className="px-4 title text-lg font-bold text-neutral-600">完了済みのタスク</p>
           <ul>
 
-            {completeTodos.map((todo) => (
+            {completeTodos.map((todo, index) => (
                 <li key={todo}>
-                  <div class="flex">
+                  <div className="flex">
                     <p>{todo}</p>
-                    <button class="px-4 py-1 ml-1 bg-neutral-200 rounded-xl hover:bg-blue-300">完了</button>
-                    <button class="px-4 py-1 ml-1 bg-neutral-200 rounded-xl hover:bg-blue-300">削除</button>
+                    <button onClick={() => onClickBack(index)} className="px-4 py-1 ml-1 bg-neutral-200 rounded-xl hover:bg-blue-300">戻す</button>
                   </div>
                 </li>
             ))}
